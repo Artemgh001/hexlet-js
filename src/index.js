bbb323ef6a7fc550ed7991a36004ac905e4a529d
Artemgh001 <a.divis@bk.ru>
2026-09-25 16:25:45 +0300

    created new dir tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import _ from "lodash";
-export default function df(){
-	console.log("Hello, Hexlet!");
-	return _.last(["one", "two"])
+function df() {
+  console.log("Hello, Hexlet!");
+  return _.last(["one", "two"]);
 }
 
+const capitalize = (text) => {
+  if (text === "") {
+    return "";
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
+export default capitalize;
